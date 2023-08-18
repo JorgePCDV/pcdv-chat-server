77c44df5c8bd0e5ee82bf2bf1a7e17a8c19bf1d7
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2023-08-18 23:27:48 +0100

    setting up server socket

--- pcdv-chat-server/pcdv-chat-server.cpp
+++ pcdv-chat-server/pcdv-chat-server.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int main()
 {
+    cout << "Step 1: Setting up DLL" << endl;
+    
     SOCKET serverSocket, acceptSocket;
     int port = 55555;
     WSADATA wsaData;
@@ -14,6 +16,33 @@ int main()
     WORD wVersionRequested = MAKEWORD(2, 2);
 
     wsaError = WSAStartup(wVersionRequested, &wsaData);
+
+    if (wsaError != 0) 
+    {
+        cout << "Winsock dll not found!" << endl;
+        return 0;
+    }
+    else 
+    {
+        cout << "Winsock dll found!" << endl;
+        cout << "Status: " << wsaData.szSystemStatus << endl;
+    }
+
+    cout << "Step 2: Setting up Server Socket" << endl;
+
+    serverSocket = INVALID_SOCKET;
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (serverSocket == INVALID_SOCKET) 
+    {
+        cout << "Error at socket():" << WSAGetLastError() << endl;
+        WSACleanup();
+        return 0;
+    }
+    else 
+    {
+        cout << "socket() is OK!" << endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
