34b3478b25a0f223e2095a052615f2e1488aa7c4
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2023-09-13 00:10:57 +0100

    receive message from client

--- pcdv-chat-server/pcdv-chat-server.cpp
+++ pcdv-chat-server/pcdv-chat-server.cpp
@@ -80,6 +80,21 @@ int main()
         return -1;
     }
     cout << "Accepted connection" << endl;
+    
+    
+    cout << "Step 6: Chat to the Client" << endl;
+    char buffer[200];
+    int byteCount = recv(acceptSocket, buffer, 200, 0);
+    if (byteCount > 0)
+    {
+        cout << "Message received: " << buffer << endl;
+    }
+    else
+    {
+        WSACleanup();
+    }
+
+    cout << "Step 7: Close Socket" << endl;
     system("pause");
     WSACleanup();
 }
